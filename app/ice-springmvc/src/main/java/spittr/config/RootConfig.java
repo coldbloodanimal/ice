244c7b08caf1ab00b385ded6d157d9cc25008bd4
coldbloodanimal <dang7758@126.com>
2017-03-15 15:07:57 +0800

    i forget

---
+++ app/ice-springmvc/src/main/java/spittr/config/RootConfig.java
@@ -0,0 +1,19 @@
+package spittr.config;
+
+import java.util.regex.Pattern;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages={"spitter"}, 
+    excludeFilters={
+        @Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
+    })
+public class RootConfig {
+}
---
+++ app/ice-springmvc/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World en ?!</h2>
+</body>
+</html>
